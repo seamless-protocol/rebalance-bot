e2d497dfaac4d8436a2efdbb0417cbd8cd5ce280
chad-js <chadflemies@gmail.com>
2025-04-09 14:28:09 -0400

    rename file

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 import { CHAIN_IDS } from "./constants/chains";
-import subscribeToLeverageTokenCreated from "./subscribers/leverageTokenCreatedSubscriber";
+import subscribeToLeverageTokenCreated from "./subscribers/leverageTokenCreated";
 
 subscribeToLeverageTokenCreated(CHAIN_IDS.BASE);
