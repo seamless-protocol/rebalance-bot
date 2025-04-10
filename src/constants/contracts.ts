4abd395da67b2b9904338071ac857a5b4cff2c6e
Danilo Kitanovic <kitanovic.danilo@outlook.com>
2025-04-10 12:48:43 +0200

    fix: remove local addresses

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,8 +1,9 @@
+import { zeroAddress } from "viem";
 import { ContractAddresses } from "../types";
 
 export const CONTRACT_ADDRESSES: Record<string, ContractAddresses> = {
   BASE: {
-    LEVERAGE_MANAGER: "0xAB8Eb9F37bD460dF99b11767aa843a8F27FB7A6e",
-    REBALANCER: "0x6858dF5365ffCbe31b5FE68D9E6ebB81321F7F86",
+    LEVERAGE_MANAGER: zeroAddress,
+    REBALANCER: zeroAddress,
   },
 };
