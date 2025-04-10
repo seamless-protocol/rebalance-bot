3bc85612c219595051ce2dbf783035b4659bf9df
chad-js <chadflemies@gmail.com>
2025-04-10 14:31:04 -0400

    rm unnecesary dotenv

--- src/utils/transactionHelpers.ts
+++ src/utils/transactionHelpers.ts
@@ -1,12 +1,9 @@
-import { createPublicClient, createWalletClient, http } from "viem";
+import { Address, createPublicClient, createWalletClient, http } from "viem";
 
 import { CHAIN } from "../constants/chain";
-import dotenv from "dotenv";
 import { privateKeyToAccount } from "viem/accounts";
 
-dotenv.config();
-
-const account = privateKeyToAccount("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80");
+const account = privateKeyToAccount(process.env.PRIVATE_KEY as Address);
 
 export const walletClient = createWalletClient({
   account,
