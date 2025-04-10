761c21f8d2a9dab6f48b72227905de43f86b8f9c
Danilo Kitanovic <kitanovic.danilo@outlook.com>
2025-04-10 10:49:54 +0200

    fix: udpate handler to check if auction is active at first place

--- src/handlers/price-change.ts
+++ src/handlers/price-change.ts
@@ -8,6 +8,7 @@ import {
 } from "../utils/contractHelpers";
 
 export async function tryCreateAuction(leverageToken: Address): Promise<RebalanceStatus> {
+  // Get rebalance status of leverage token, token can be eligible for Dutch auction or pre-liquidation or not eligible at all
   const rebalanceStatus = await rebalancerContract.read.getRebalanceStatus([
     leverageManagerContract.address,
     leverageToken,
@@ -15,19 +16,31 @@ export async function tryCreateAuction(leverageToken: Address): Promise<Rebalanc
 
   console.log(`Rebalance status for leverage token (${leverageToken}):`, rebalanceStatus);
 
+  // If token is eligible for Dutch auction, create auction so rebalancer can compete and rebalance token
   if (rebalanceStatus === RebalanceStatus.DUTCH_ELIGIBLE) {
-    console.log(`Leverage token (${leverageToken}) is eligible for Dutch auction`);
-    console.log(`Creating auction for leverage token (${leverageToken})...`);
+    console.log(`Checking if leverage token (${leverageToken}) already has an active valid auction...`);
 
+    // We need to check if valid auction already exists in case someone else already created auction
+    // If auction is not valid, we will create new one otherwise we will skip creating auction because it will revert anyway
     const rebalanceAdapterContract = getLeverageTokenRebalanceAdapterContract(leverageToken);
-    const tx = await rebalanceAdapterContract.write.createAuction();
-    console.log(`Created auction for leverage token (${leverageToken}), waiting for receipt:`, tx);
+    const doesValidAuctionExists = await rebalanceAdapterContract.read.isAuctionValid();
 
-    await publicClient.waitForTransactionReceipt({
-      hash: tx,
-    });
-    console.log(`Auction created for leverage token (${leverageToken})`);
+    if (!doesValidAuctionExists) {
+      console.log(`Creating auction for leverage token (${leverageToken})...`);
+
+      const tx = await rebalanceAdapterContract.write.createAuction();
+      console.log(`Created auction for leverage token (${leverageToken}), waiting for receipt:`, tx);
+
+      await publicClient.waitForTransactionReceipt({
+        hash: tx,
+      });
+      console.log(`Auction created for leverage token (${leverageToken})`);
+    }
   }
+
+  // In case of Dutch auction eligibility we will return than enum here no matter did we create auction or not
+  // It is just important that return value indicates that auction is in progress and competition is happening
+  // If auction is not created, it means that auction is not eligible for Dutch auction and we will not compete for it
 
   return rebalanceStatus;
 }
