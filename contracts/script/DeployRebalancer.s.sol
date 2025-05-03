c10878560d14783156b46fc474314d1c96141bd7
Danilo Kitanovic <kitanovic.danilo@outlook.com>
2025-05-03 13:29:46 +0200

    fixes

--- contracts/script/DeployRebalancer.s.sol
+++ contracts/script/DeployRebalancer.s.sol
@@ -7,9 +7,9 @@ import {Script} from "forge-std/Script.sol";
 import {Rebalancer} from "src/Rebalancer.sol";
 
 contract DeployRebalancer is Script {
-    address public constant LEVERAGE_MANAGER = 0xC070A317F23E9A4e982e356485416251dd3Ed944;
-    address public constant SWAP_ADAPTER = 0x380de6769c9ECC5e8D032F8a3904bF17DC852cA5;
-    address public constant MORPHO = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
+    address public constant LEVERAGE_MANAGER = 0x0000000000000000000000000000000000000000;
+    address public constant SWAP_ADAPTER = 0x0000000000000000000000000000000000000000;
+    address public constant MORPHO = 0x0000000000000000000000000000000000000000;
 
     function run() public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
