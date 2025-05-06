0a67e8a2054e8f01ebcffd552a28a72154b5edd5
Danilo Kitanovic <kitanovic.danilo@outlook.com>
2025-05-06 20:50:43 +0200

    feat: lifi api key

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -3,3 +3,5 @@ import { parseEther } from "viem";
 export const BASE_RATIO = parseEther("1");
 export const DEFAULT_DUTCH_AUCTION_STEP_COUNT = 10;
 export const DEFAULT_DUTCH_AUCTION_POLLING_INTERVAL = 10_000;
+
+export const LIFI_API_URL = "https://li.quest/v1/quote";
--- src/services/routing/lifi.ts
+++ src/services/routing/lifi.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
 import { CONTRACT_ADDRESSES } from "../../constants/contracts";
+import { LIFI_API_URL } from "../../constants/values";
 import { GetLIFIQuoteInput } from "../../types";
 
 export const getLIFIQuote = async (args: GetLIFIQuoteInput) => {
   const { fromToken, toToken, fromAmount } = args;
 
-  const result = await axios.get("https://li.quest/v1/quote", {
+  const result = await axios.get(LIFI_API_URL, {
     params: {
       fromChain: 8453,
       toChain: 8453,
@@ -13,6 +14,9 @@ export const getLIFIQuote = async (args: GetLIFIQuoteInput) => {
       toToken,
       fromAmount,
       fromAddress: CONTRACT_ADDRESSES.REBALANCER,
+    },
+    headers: {
+      "x-lifi-api-key": process.env.LIFI_API_KEY,
     },
   });
 
