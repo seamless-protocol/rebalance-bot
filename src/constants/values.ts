1f04f36daa7254fa863e401d0bef9d0d8649c2b2
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2025-05-08 17:16:08 +0200

    feat: clear interval (#52)
       - in parallel with checking rebalance profitability check if auction is still valid and close interval if no
       - when create auction event is received close interval if outdated interval is active

--- src/constants/values.ts
+++ src/constants/values.ts
@@ -1,4 +1,7 @@
-import { parseEther } from "viem";
+import { Address, parseEther } from "viem";
+
+// Map of active intervals that are trying to take on dutch auction for specific rebalance adapter
+export const DUTCH_AUCTION_ACTIVE_INTERVALS = new Map<Address, any>();
 
 export const BASE_RATIO = parseEther("1");
 export const DEFAULT_DUTCH_AUCTION_STEP_COUNT = 10;
