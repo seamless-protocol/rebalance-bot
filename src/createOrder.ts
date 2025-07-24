1eeabb75fa8097424b761dce6fd6e1ee6ce8bc7f
Faisal Usmani <faisal.of.usmani@gmail.com>
2025-07-24 11:18:00 -0400

    Update flashloan to use aave

--- src/createOrder.ts
+++ src/createOrder.ts
@@ -8,6 +8,7 @@ import {
   SwapAdvancedSettings,
 } from "@cowprotocol/cow-sdk";
 import LeverageManagerAbi from "./leverageManagerAbi.json";
+import ERC20Abi from "./erc20Abi.json";
 
 import { config } from "dotenv";
 config();
@@ -29,9 +30,11 @@ const greet = async (name: string) => {
   const weETHAddress = "0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A";
   const WETHAddress = "0x4200000000000000000000000000000000000006";
   const leverageTokenAddress = "0xA2fceEAe99d2cAeEe978DA27bE2d95b0381dBB8c";
+  const settlementContractAddress = "0x9008D19f58AAbD9eD0D60971565AA8510560ab41";
 
   const provider = new ethers.providers.JsonRpcProvider("https://mainnet.base.org");
   const signer = new ethers.Wallet(process.env.PRIVATE_KEY as string, provider);
+  const weETH = new ethers.Contract(weETHAddress, ERC20Abi, provider);
   const leverageManager = new ethers.Contract(leverageManagerAddress, LeverageManagerAbi, provider);
 
   // 100000000000000 = 0.0001 weETH
@@ -47,22 +50,35 @@ const greet = async (name: string) => {
   console.log("tokenFee", tokenFee);
   console.log("treasuryFee", treasuryFee);
 
+  const transferCollateralTx = weETH.interface.encodeFunctionData("transferFrom", [
+    signer.address,
+    settlementContractAddress,
+    equityInCallateral.mul(10010).div(10000),
+  ]);
+
   const mintTx = leverageManager.interface.encodeFunctionData("mint", [
     leverageTokenAddress,
-    equityInCallateral,
+    equityInCallateral.mul(10010).div(10000),
     shares,
   ]);
 
   const appData = {
     metadata: {
       flashloan: {
-        // morpho vault
-        lender: "0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb",
+        // aave vault
+        lender: "0xA238Dd80C259a72e81d7e4664a9801593F98d1c5",
+        borrower: settlementContractAddress,
         token: weETHAddress,
-        amount: collateral.sub(equity).toString(), // 5,000 USDT
+        amount: collateral.sub(equity).toString(),
       },
       hooks: {
         pre: [
+          {
+            target: weETHAddress,
+            value: "0",
+            callData: transferCollateralTx,
+            gasLimit: "90000",
+          },
           {
             target: leverageManagerAddress,
             value: "0",
@@ -89,10 +105,10 @@ const greet = async (name: string) => {
     sellTokenDecimals: 18,
     buyToken: weETHAddress,
     buyTokenDecimals: 18,
-    amount: debt.toString(), // swap the WETH for weETH
+    amount: collateral.sub(equity).mul(10005).div(10000).toString(), // flashloan amount plus 0.05% Aave flash loan fee
     // receiver is always the settlement contract because the driver takes
     // funds from the settlement contract to pay back the loan
-    receiver: "0x9008D19f58AAbD9eD0D60971565AA8510560ab41", // cow settlement contract address on sepolia
+    receiver: settlementContractAddress, // cow settlement contract address on base
   };
 
   const quote = await sdk.getQuote(parameters, {
