811d10458adbd40c4c7cb1df6f2914fa17d95461
Chad <22300733+chad-js@users.noreply.github.com>
2025-09-30 11:35:53 -0400

    feat: support deploy scripts for multiple chains (#83)

---
+++ contracts/script/1/DeployConstants.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+library DeployConstants {
+    address public constant OWNER = 0x0000000000000000000000000000000000000000;
+
+    address public constant MORPHO = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
+
+    address public constant LEVERAGE_MANAGER = 0x5C37EB148D4a261ACD101e2B997A0F163Fb3E351;
+}
---
+++ contracts/script/8453/DeployRebalancer.s.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {console} from "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+
+import {DutchAuctionRebalancer} from "src/DutchAuctionRebalancer.sol";
+import {PreLiquidationRebalancer} from "src/PreLiquidationRebalancer.sol";
+import {DeployConstants} from "./DeployConstants.sol";
+
+contract DeployRebalancer is Script {
+    address public immutable OWNER = DeployConstants.OWNER;
+    address public immutable LEVERAGE_MANAGER = DeployConstants.LEVERAGE_MANAGER;
+    address public immutable MORPHO = DeployConstants.MORPHO;
+
+    function run() public {
+        require(OWNER != address(0), "OWNER address is not set");
+
+        address deployerAddress = msg.sender;
+
+        console.log("Deployer address: ", deployerAddress);
+        console.log("Deployer balance: ", deployerAddress.balance);
+        console.log("BlockNumber: ", block.number);
+        console.log("ChainId: ", block.chainid);
+
+        console.log("Owner: ", OWNER);
+        console.log("LeverageManager: ", LEVERAGE_MANAGER);
+        console.log("Morpho: ", MORPHO);
+
+        console.log("Deploying...");
+
+        vm.startBroadcast();
+
+        PreLiquidationRebalancer preLiquidationRebalancer =
+            new PreLiquidationRebalancer(OWNER, LEVERAGE_MANAGER, MORPHO);
+        console.log("PreLiquidationRebalancer deployed to:", address(preLiquidationRebalancer));
+
+        DutchAuctionRebalancer dutchAuctionRebalancer = new DutchAuctionRebalancer(OWNER, LEVERAGE_MANAGER, MORPHO);
+        console.log("DutchAuctionRebalancer deployed to:", address(dutchAuctionRebalancer));
+
+        vm.stopBroadcast();
+    }
+}
