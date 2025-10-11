7abdb323be76d035953ee29e7735694a9a3efaff
chad-js <chadflemies@gmail.com>
2025-10-11 11:07:41 -0400

    add name env var

--- src/constants/slack.ts
+++ src/constants/slack.ts
@@ -4,3 +4,4 @@ dotenv.config();
 
 export const SLACK_ALERT_CHANNEL_ID = process.env.SLACK_ALERT_CHANNEL_ID || "";
 export const SLACK_AUTH_TOKEN = process.env.SLACK_AUTH_TOKEN || "";
+export const SLACK_REBALANCE_BOT_NAME = process.env.SLACK_REBALANCE_BOT_NAME || "";
--- src/utils/alerts.ts
+++ src/utils/alerts.ts
@@ -1,5 +1,5 @@
 import { CHAIN_ID } from "../constants/chain";
-import { SLACK_ALERT_CHANNEL_ID, SLACK_AUTH_TOKEN } from "../constants/slack";
+import { SLACK_ALERT_CHANNEL_ID, SLACK_AUTH_TOKEN, SLACK_REBALANCE_BOT_NAME } from "../constants/slack";
 
 import { LogLevel } from "../types";
 import { WebClient } from "@slack/web-api";
@@ -9,6 +9,7 @@ const slackClient = new WebClient(SLACK_AUTH_TOKEN);
 export const sendAlert = async (message: string, logLevel: LogLevel) => {
   await notifySlackChannel(message, logLevel);
 };
+
 
 const notifySlackChannel = async (message: string, logLevel: LogLevel) => {
   if (!SLACK_ALERT_CHANNEL_ID || !SLACK_AUTH_TOKEN) {
@@ -20,7 +21,7 @@ const notifySlackChannel = async (message: string, logLevel: LogLevel) => {
 
     await slackClient.chat.postMessage({
       channel: SLACK_ALERT_CHANNEL_ID,
-      text: `${getSymbol(logLevel)} ${message}\n• Chain ID: \`${CHAIN_ID}\``,
+      text: `${getSymbol(logLevel)} ${message}\n• Chain ID: \`${CHAIN_ID}\`\n• Rebalance Bot: \`${SLACK_REBALANCE_BOT_NAME}\``,
     });
   } catch (error) {
     console.error("Error notifying slack channel:", error);
